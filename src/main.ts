3dcf61aadcda2741dd321c3e6db1005cc7be26c6
LokmanRazu <lokmananil65@gmail.com>
2023-06-02 22:58:50 +0600

    ver5

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { DocumentBuilder,SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
  
-  const app = await NestFactory.create(AppModule,{cors:true});
+  const app = await NestFactory.create(AppModule);
   app.enableCors();
   const config = new DocumentBuilder()
   .setTitle('Final-Project')
